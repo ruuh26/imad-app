bb327313ff3d60a6ebc6775c1a58ac5aa6d66b75
ruuh26 <30907783+ruuh26@users.noreply.github.com>
2017-08-13 12:27:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article 1', function (req,res){
+app.get('/article1', function (req,res){
     res.send('article one is served now');
 });
 app.get('/ui/style.css', function (req, res) {
