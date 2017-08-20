88bd14664780500326df7d486b4f0e585135fcc0
ruuh26 <30907783+ruuh26@users.noreply.github.com>
2017-08-21 00:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/badiwelcome', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'badiwelcome.html'));
 });
 
+app.get('/badihbd', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'badihbd.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
